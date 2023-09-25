c41c1bf6d0af4b8c97d1cba25b940e211a180a7f
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-25 21:53:47 +0600

    banner title is written in banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -6,6 +6,8 @@ const Banner = () => {
             <div className=" bg-[url('https://i.ibb.co/tL4YRtn/help-people.jpg')] bg-cover opacity-25 h-[70vh]">
                 
             </div>
+            <h1 className="text-5xl font-bold absolute top-40 left-72 ">I Grow By Helping People In Need</h1>
+
             <div className="absolute top-60 left-[500px] flex">
             <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
             <button className="btn ml-2 bg-[#FF444A] text-white">Search</button>

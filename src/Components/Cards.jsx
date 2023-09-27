fa9c57b6c647d8a13d219318e84a411eb37a26bb
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-27 17:55:56 +0600

    search button is working well

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
-const Cards = ({cards}) => {
+const Cards = ({ cards, mySearchKey }) => {
+
+    const [displayCard, setDisplayCard] = useState([]);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        setSearch(mySearchKey)
+        const selectedCards = cards.filter(card => card.category.toLowerCase().indexOf(mySearchKey.toLowerCase()) != -1);
+        setDisplayCard(selectedCards)
+
+    }, [mySearchKey, cards])
+
+
+    useEffect(() => {
+        setSearch(mySearchKey)
+        setDisplayCard(cards)
+
+    }, [])
+
+
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container mx-auto gap-4 py-10'>
             {
-                cards?.map((card)=> <Card key={card.id} card={card}></Card>)
+                displayCard?.map((card) => <Card key={card.id} card={card}></Card>)
             }
         </div>
     );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,24 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import Cards from "./Cards";
+import { useState } from "react";
 
 
 const Home = () => {
 
+    const [mySearchKey, setMySearchKey] = useState('');
+
     const cards = useLoaderData();
 
+    const handleSearch = (event) =>{
+        event.preventDefault()
+        const search = event.target.text.value.toLowerCase().trim();
+        setMySearchKey(search);
+    }
     return (
         <div>
-            <Banner></Banner>
-            <Cards cards={cards}></Cards>
+            <Banner handleSearch={handleSearch}></Banner>
+            <Cards mySearchKey={mySearchKey} cards={cards}></Cards>
         </div>
     );
 };
